35282a639e23076c800c961ab665484af723e7e3
nkoenig <nkoenig@willowgarage.com>
2011-10-18 07:22:10 -0700

    Removed kitchen

---
+++ src/gui/WorldPropertiesWidget.hh
@@ -0,0 +1,130 @@
+#ifndef EDIT_SCENE_WIDGET_HH
+#define EDIT_SCENE_WIDGET_HH
+
+#include <QWidget>
+#include <QPushButton>
+#include <QFrame>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+#include <QComboBox>
+#include <QGroupBox>
+
+#include "physics/PhysicsTypes.hh"
+#include "transport/TransportTypes.hh"
+#include "math/Vector3.hh"
+#include "msgs/msgs.h"
+
+namespace gazebo
+{
+  namespace msgs
+  {
+    class RequestMsg;
+  }
+
+  namespace gui
+  {
+    class SceneWidget;
+    class PhysicsWidget;
+
+    class WorldPropertiesWidget : public QWidget
+    {
+      Q_OBJECT
+      public: WorldPropertiesWidget( QWidget *parent = 0 );
+      public: virtual ~WorldPropertiesWidget();
+
+      protected: void closeEvent(QCloseEvent * /*_event*/);
+      protected: void showEvent(QShowEvent * /*_event*/);
+
+      private: SceneWidget *sceneWidget;
+      private: PhysicsWidget *physicsWidget;
+    };
+
+
+    class PhysicsWidget : public QWidget
+    {
+      Q_OBJECT
+      public: PhysicsWidget( QWidget *parent = 0 );
+      public: virtual ~PhysicsWidget();
+
+      public: void Init();
+
+      private slots: void OnSolverType(int _index);
+
+      private slots: void OnGravity();
+      private slots: void OnDt();
+      private slots: void OnSOR();
+      private slots: void OnIters();
+      private slots: void OnCFM();
+      private slots: void OnERP();
+      private slots: void OnMaxVel();
+      private slots: void OnSurfaceLayer();
+
+      private: void OnResponse(const boost::shared_ptr<msgs::Response const> &_msg);
+
+      private: transport::NodePtr node;
+      private: transport::PublisherPtr physicsPub, requestPub;
+      private: transport::SubscriberPtr responseSub;
+      private: msgs::Request *requestMsg;
+      private: bool initialized;
+
+      private: QLineEdit *gravityXLineEdit;
+      private: QLineEdit *gravityYLineEdit;
+      private: QLineEdit *gravityZLineEdit;
+      private: QComboBox *solverTypeBox;
+      private: QLineEdit *dtLineEdit;
+      private: QLineEdit *itersLineEdit;
+      private: QLineEdit *sorLineEdit;
+      private: QLineEdit *cfmLineEdit;
+      private: QLineEdit *erpLineEdit;
+      private: QLineEdit *maxVelLineEdit;
+      private: QLineEdit *surfaceLayerLineEdit;
+    };
+
+
+    class SceneWidget : public QWidget
+    {
+      Q_OBJECT
+      public: SceneWidget( QWidget *parent = 0 );
+      public: virtual ~SceneWidget();
+      public: void Init();
+
+      private slots: void OnAmbientColor();
+      private slots: void OnBackgroundColor();
+
+      private slots: void OnFogColor();
+      private slots: void OnFogStart();
+      private slots: void OnFogEnd();
+      private slots: void OnFogDensity(double _density);
+      private slots: void OnFogType(int _index);
+      private slots: void OnFogToggle(bool _value);
+
+      private slots: void OnShadows(bool _state=false);
+
+      private: void OnResponse(const boost::shared_ptr<msgs::Response const> &_msg);
+
+      public: bool initialized;
+      private: QPushButton *ambientColorButton;
+      private: QFrame *ambientColorFrame;
+
+      private: QPushButton *backgroundColorButton;
+      private: QFrame *backgroundColorFrame;
+
+      private: QCheckBox *shadowsButton;
+
+      private: QGroupBox *fogBox;
+      private: QPushButton *fogColorButton;
+      private: QLineEdit *fogStart;
+      private: QLineEdit *fogEnd;
+      private: QDoubleSpinBox *fogDensitySpin;
+      private: QComboBox *fogTypeBox;
+
+      private: transport::NodePtr node;
+      private: transport::PublisherPtr scenePub, requestPub;
+      private: transport::SubscriberPtr responseSub;
+      private: msgs::Request *requestMsg;
+    };
+  }
+}
+
+#endif
---
+++ src/rendering/GUIOverlay.cc
@@ -0,0 +1,238 @@
+#include "rendering/GUIOverlay.hh"
+#include "common/Console.hh"
+
+#ifdef HAVE_CEGUI
+#include "CEGUI/CEGUI.h"
+#include "CEGUI/RendererModules/Ogre/CEGUIOgreRenderer.h"
+#endif
+
+#include "common/SystemPaths.hh"
+#include "msgs/msgs.h"
+#include "transport/Transport.hh"
+#include "transport/Node.hh"
+#include "math/Vector2d.hh"
+
+#include "rendering/ogre.h"
+#include "rendering/RenderTypes.hh"
+#include "rendering/Camera.hh"
+#include "rendering/DepthCamera.hh"
+
+using namespace gazebo;
+using namespace rendering;
+
+GUIOverlay::GUIOverlay()
+{
+  this->connections.push_back( 
+      event::Events::ConnectPreRender( 
+        boost::bind(&GUIOverlay::PreRender, this) ) );
+}
+
+GUIOverlay::~GUIOverlay()
+{
+#ifdef HAVE_CEGUI
+  CEGUI::OgreRenderer::destroySystem();
+#endif
+}
+
+void GUIOverlay::Init( Ogre::RenderTarget *_renderTarget )
+{
+#ifdef HAVE_CEGUI
+  CEGUI::System::getSingletonPtr();
+
+  std::string logPath = common::SystemPaths::Instance()->GetLogPath();
+  logPath += "/cegui.log";
+
+  this->guiRenderer = &CEGUI::OgreRenderer::create(*_renderTarget);
+  CEGUI::OgreResourceProvider &ip = CEGUI::OgreRenderer::createOgreResourceProvider();
+  CEGUI::OgreImageCodec &ic = CEGUI::OgreRenderer::createOgreImageCodec();
+
+  CEGUI::System::create(*((CEGUI::Renderer*)this->guiRenderer), 
+      (CEGUI::ResourceProvider*)(&ip), 
+      static_cast<CEGUI::XMLParser*>(0), 
+      (CEGUI::ImageCodec*)(&ic),
+      NULL, "", logPath);
+
+  CEGUI::Imageset::setDefaultResourceGroup("Imagesets");
+  CEGUI::Font::setDefaultResourceGroup("Fonts");
+  CEGUI::Scheme::setDefaultResourceGroup("Schemes");
+  CEGUI::WidgetLookManager::setDefaultResourceGroup("LookNFeel");
+  CEGUI::WindowManager::setDefaultResourceGroup("Layouts");
+
+  CEGUI::SchemeManager::getSingleton().create("TaharezLook.scheme");
+  CEGUI::SchemeManager::getSingleton().create("VanillaSkin.scheme");
+  CEGUI::FontManager::getSingleton().create("DejaVuSans-10.font");
+  //CEGUI::System::getSingleton().setDefaultMouseCursor("TaharezLook", "MouseArrow");
+
+  // clearing this queue actually make sure it's created
+  this->guiRenderer->getDefaultRenderingRoot().clearGeometry(CEGUI::RQ_OVERLAY);
+
+  // Create a root window, and set it as the root window
+  CEGUI::Window *rootWindow  = CEGUI::WindowManager::getSingleton().createWindow("DefaultWindow", "root");
+  CEGUI::System::getSingleton().setGUISheet( rootWindow );
+#endif
+}
+
+void GUIOverlay::CreateWindow( const std::string &_type, 
+                               const std::string &_name,
+                               const std::string &_parent,
+                               const math::Vector2d &_position, 
+                               const math::Vector2d &_size,
+                               const std::string &_text)
+{
+#ifdef HAVE_CEGUI
+
+  CEGUI::Window *parent = CEGUI::WindowManager::getSingleton().getWindow(_parent);
+  CEGUI::Window *window = CEGUI::WindowManager::getSingleton().createWindow(_type, _name);
+
+  parent->addChildWindow( window );
+
+  window->setPosition( CEGUI::UVector2( CEGUI::UDim(_position.x, 0), 
+                                             CEGUI::UDim(_position.y, 0) ) );
+
+  window->setSize( CEGUI::UVector2( CEGUI::UDim(_size.x, 0), 
+                                         CEGUI::UDim(_size.y, 0) ) );
+  window->setText( _text );
+#endif
+}
+
+bool GUIOverlay::HandleMouseEvent( const common::MouseEvent &/*_evt*/)
+{
+  bool result = false;
+#ifdef HAVE_CEGUI
+  //CEGUI::System *system = CEGUI::System::getSingletonPtr();
+  //result = result || system->injectMousePosition( _evt.pos.x, _evt.pos.y);
+/*
+  if (_evt. == common::MouseEvent::DOWN)
+    result = result || system->injectMouseButtonDown( CEGUI::LeftButton);
+  else if (_evt.left == common::MouseEvent::UP)
+    result = result || system->injectMouseButtonUp( CEGUI::LeftButton);
+
+  if (_evt.right == common::MouseEvent::DOWN)
+    result = result || system->injectMouseButtonDown( CEGUI::RightButton);
+  else if (_evt.right == common::MouseEvent::UP)
+    result = result ||system->injectMouseButtonUp( CEGUI::RightButton);
+    */
+#endif
+
+  return result;
+}
+
+bool GUIOverlay::IsInitialized() 
+{
+#ifdef HAVE_CEGUI
+  return CEGUI::WindowManager::getSingletonPtr() != NULL;
+#else
+  return false;
+#endif
+}
+  
+/// Load a CEGUI layout file
+void GUIOverlay::LoadLayout( const std::string &_filename )
+{
+  this->layoutFilename = _filename;
+}
+
+void GUIOverlay::PreRender()
+{
+#ifdef HAVE_CEGUI
+  if (this->IsInitialized() && !this->layoutFilename.empty())
+  {
+    this->LoadLayoutImpl( this->layoutFilename );
+    this->layoutFilename.clear();
+  }
+#endif
+}
+
+/// Load a CEGUI layout file
+#ifdef HAVE_CEGUI
+CEGUI::Window *GUIOverlay::LoadLayoutImpl( const std::string &_filename )
+{
+  CEGUI::Window *window = NULL;
+  CEGUI::Window *rootWindow = NULL;
+
+  CEGUI::WindowManager *windowManager = CEGUI::WindowManager::getSingletonPtr();
+  if (!windowManager)
+  {
+    gzerr << "Attempting to create a GUI overlay window before load\n";
+    return window;
+  }
+
+  rootWindow = windowManager->getWindow("root");
+  if (rootWindow)
+  {
+    window = windowManager->loadWindowLayout( _filename );
+    rootWindow->addChildWindow( window );
+  }
+  else
+  {
+    gzerr << "Attempting to create a GUI overlay window before load\n";
+  }
+
+  return window;
+}
+#endif
+
+void GUIOverlay::Resize( unsigned int _width, unsigned int _height )
+{
+#ifdef HAVE_CEGUI
+  this->guiRenderer->setDisplaySize( CEGUI::Size(_width, _height) );
+
+  CEGUI::WindowManager *windowManager = CEGUI::WindowManager::getSingletonPtr();
+
+  CEGUI::Window *rootWindow = windowManager->getWindow("root");
+  rootWindow->setArea( CEGUI::UDim(0,0), CEGUI::UDim(0,0), CEGUI::UDim(1,0), CEGUI::UDim(1,0));
+#endif
+}
+
+bool GUIOverlay::AttachCameraToImage(DepthCameraPtr &_camera, 
+                                     const std::string &_windowName)
+{
+  CameraPtr cam = boost::shared_dynamic_cast<Camera>(_camera);
+  return this->AttachCameraToImage(cam , _windowName);
+
+}
+
+bool GUIOverlay::AttachCameraToImage(CameraPtr &_camera, const std::string &_windowName)
+{
+#ifdef HAVE_CEGUI
+  CEGUI::Window *window = NULL;
+  CEGUI::WindowManager *windowManager = CEGUI::WindowManager::getSingletonPtr();
+
+  if (!windowManager)
+  {
+    gzerr << "CEGUI system not initialized\n";
+    return false;
+  }
+
+  window = windowManager->getWindow(_windowName);
+  if (!window)
+  {
+    gzerr << "Unable to find window[" << _windowName << "]\n";
+    return false;
+  }
+
+  if (!_camera->GetRenderTexture())
+  {
+    gzerr << "Camera does not have a render texture\n";
+    return false;
+  }
+
+#ifdef HAVE_CEGUI
+  Ogre::TexturePtr texPtr(_camera->GetRenderTexture());
+  CEGUI::Texture &guiTex = this->guiRenderer->createTexture(
+      texPtr );
+
+  CEGUI::Imageset &imageSet = CEGUI::ImagesetManager::getSingleton().create("RTTImageset", guiTex);
+
+  imageSet.defineImage("RTTImage", CEGUI::Point(0.0f, 0.0f),
+      CEGUI::Size(guiTex.getSize().d_width, 
+                  guiTex.getSize().d_height),
+      CEGUI::Point(0.0f, 0.0f));
+
+  window->setProperty("Image", CEGUI::PropertyHelper::imageToString(&imageSet.getImage("RTTImage")));
+  return true;
+
+#endif
+#endif
+  return false;
+}
