db2f1137a4f6c7dbacbf05bdd2f853513597c34a
Nate Koenig <nkoenig@willowgarage.com>
2011-11-28 10:34:09 -0800

    Updates to loading world files

---
+++ src/transport/SubscribeOptions.hh
@@ -0,0 +1,70 @@
+#ifndef SUBSCRIBEOPTIONS_HH
+#define SUBSCRIBEOPTIONS_HH
+
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include "transport/CallbackHelper.hh"
+
+namespace gazebo
+{
+  namespace transport
+  {
+    /// \addtogroup gazebo_transport
+    /// \{
+
+    /// \brief Options for a subscription
+    class SubscribeOptions
+    {
+      public: SubscribeOptions()
+              {}
+
+      public: template<class M>
+              void Init(const std::string &_topic, 
+                        /*const boost::function<
+                        void (const boost::shared_ptr<M const> &)> &_callback,*/
+                        NodePtr _node)
+              {
+                google::protobuf::Message *msg = NULL;
+                M msgtype;
+                msg = dynamic_cast<google::protobuf::Message *>(&msgtype);
+                if (!msg)
+                  gzthrow("Subscribe requires a google protobuf type");
+
+                this->node = _node; 
+                this->topic = _topic;
+                this->msgType = msg->GetTypeName();
+                  //this->subscription = CallbackHelperPtr( 
+                      //new CallbackHelperT<M>(_callback) );
+              }
+
+      public: NodePtr GetNode() const
+              {
+                return this->node;
+              }
+
+      public: std::string GetTopic() const
+              {
+                return this->topic;
+              }
+
+      public: std::string GetMsgType() const
+              {
+                return this->msgType;
+              }
+
+      /*public: CallbackHelperPtr GetSubscription() const
+              {
+                return this->subscription;
+              }
+              */
+
+      private: std::string topic;
+      private: std::string msgType;
+      //private: CallbackHelperPtr subscription;
+      private: NodePtr node;
+    };
+    /// \}
+  }
+}
+
+#endif
