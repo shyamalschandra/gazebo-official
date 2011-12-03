571598ed89dc682f7c845f0662b1a9d5a544341d
Nate Koenig <nkoenig@willowgarage.com>
2011-12-03 08:19:30 -0800

    Fixed regression tests

---
+++ test/regression/speed.cc
@@ -0,0 +1,83 @@
+#include <gtest/gtest.h>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+
+#include "src/Server.hh"
+#include "transport/TransportTypes.hh"
+#include "transport/Node.hh"
+
+
+using namespace gazebo;
+
+double g_percent = 0;
+
+Server *server = NULL;
+std::string g_worldFile;
+
+void OnStats(const boost::shared_ptr<msgs::WorldStatistics const> &_msg)
+{
+  common::Time simTime  = msgs::Convert( _msg->sim_time() );
+  common::Time realTime = msgs::Convert( _msg->real_time() );
+
+  if (simTime.Double() > 0)
+    g_percent  = (simTime / realTime).Double();
+}
+
+void RunServer()
+{
+  ASSERT_NO_THROW(server = new Server());
+  ASSERT_NO_THROW(server->Load(g_worldFile));
+  ASSERT_NO_THROW(server->Init());
+
+  server->Run();
+  ASSERT_NO_THROW(server->Fini());
+  delete server;
+}
+
+void SpeedTest(double minSpeed)
+{
+  boost::thread thread(boost::bind(RunServer));
+  while (!server || !server->IsRunning())
+    usleep(1000000);
+
+  usleep(1000000);
+  transport::NodePtr node = transport::NodePtr( new transport::Node() );
+  ASSERT_NO_THROW(node->Init());
+  transport::SubscriberPtr sub = node->Subscribe("~/world_stats", OnStats);
+
+  g_percent = -1;
+  while (g_percent < 0)
+    usleep(100000);
+
+  EXPECT_GT(g_percent, minSpeed);
+
+  ASSERT_NO_THROW(server->Stop());
+  thread.join();
+
+  usleep(1000000);
+}
+
+TEST(Speed, EmptyWorld)
+{
+  g_worldFile = "worlds/empty.world";
+  SpeedTest(3800.0);
+}
+
+TEST(Speed, ShapesWorld)
+{
+  g_worldFile = "worlds/shapes.world";
+  SpeedTest(120.0);
+}
+
+TEST(Speed, PR2World)
+{
+  g_worldFile = "worlds/pr2.world";
+  SpeedTest(4.0);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
