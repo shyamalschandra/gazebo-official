71a80b5cc01419796c7ba56f8a33c5ed0b3ee7fe
mihai.dolha <dolha@cs.tum.edu>
2012-05-18 09:45:19 -0700

    working on sync problems for animations

---
+++ src/gui/JointControlWidget.cc
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "transport/Node.hh"
+#include "transport/Transport.hh"
+#include "gui/JointControlWidget.hh"
+
+using namespace gazebo;
+using namespace gui;
+
+/////////////////////////////////////////////////
+JointForceControl::JointForceControl(const std::string &_name, QWidget *_parent)
+  : QWidget(_parent), name(_name)
+{
+  QHBoxLayout *mainLayout = new QHBoxLayout;
+
+  QDoubleSpinBox *forceSpin = new QDoubleSpinBox;
+  forceSpin->setRange(-100.0, 100.0);
+  forceSpin->setSingleStep(0.001);
+  forceSpin->setDecimals(3);
+  forceSpin->setValue(0.000);
+
+  mainLayout->addWidget(new QLabel(tr(_name.c_str())), 0);
+  mainLayout->addItem(new QSpacerItem(10, 20, QSizePolicy::Expanding,
+                                      QSizePolicy::Minimum));
+  mainLayout->addWidget(forceSpin, 0);
+  mainLayout->addWidget(new QLabel("N-m", this), 0);
+  this->setLayout(mainLayout);
+
+  connect(forceSpin, SIGNAL(valueChanged(double)),
+        this, SLOT(OnChanged(double)));
+}
+
+/////////////////////////////////////////////////
+void JointForceControl::OnChanged(double _value)
+{
+  emit changed(_value, this->name);
+}
+
+/////////////////////////////////////////////////
+JointPIDControl::JointPIDControl(const std::string &_name, QWidget *_parent)
+  : QWidget(_parent), name(_name)
+{
+  QHBoxLayout *mainLayout = new QHBoxLayout;
+
+  QDoubleSpinBox *posSpin = new QDoubleSpinBox;
+  posSpin->setRange(-3.1415, 3.1415);
+  posSpin->setSingleStep(0.001);
+  posSpin->setDecimals(3);
+  posSpin->setValue(0.000);
+
+  mainLayout->addWidget(new QLabel(tr(_name.c_str())), 0);
+  mainLayout->addItem(new QSpacerItem(10, 20, QSizePolicy::Expanding,
+                                      QSizePolicy::Minimum));
+  mainLayout->addWidget(posSpin, 0);
+  mainLayout->addWidget(new QLabel("radians", this), 0);
+  this->setLayout(mainLayout);
+
+  connect(posSpin, SIGNAL(valueChanged(double)),
+        this, SLOT(OnChanged(double)));
+}
+
+/////////////////////////////////////////////////
+void JointPIDControl::OnChanged(double _value)
+{
+  emit changed(_value, this->name);
+}
+
+/////////////////////////////////////////////////
+JointControlWidget::JointControlWidget(const std::string &_modelName,
+                                       QWidget *_parent)
+  : QWidget(_parent)
+{
+  this->setWindowTitle("Joint Control");
+  this->node = transport::NodePtr(new transport::Node());
+  this->node->Init();
+  this->jointPub = this->node->Advertise<msgs::JointCmd>(
+      std::string("~/") + _modelName + "/joint_cmd");
+
+  this->requestMsg = msgs::CreateRequest("entity_info");
+  this->requestMsg->set_data(_modelName);
+
+  msgs::Response response = transport::request("default", *this->requestMsg);
+
+  msgs::Model modelMsg;
+  modelMsg.ParseFromString(response.serialized_data());
+
+
+  // Create the Force control scroll area
+  QScrollArea *scrollArea = new QScrollArea;
+  QFrame *frame = new QFrame;
+  frame->setLineWidth(0);
+  frame->setFrameShadow(QFrame::Sunken);
+  frame->setFrameShape(QFrame::Box);
+
+  QVBoxLayout *vLayout = new QVBoxLayout;
+  for (int i = 0; i < modelMsg.joint_size(); ++i)
+  {
+    std::string jointName =modelMsg.joint(i).name();
+    JointForceControl *slider = new JointForceControl(jointName, this);
+    this->sliders[jointName] = slider;
+    vLayout->addWidget(slider);
+    connect(slider, SIGNAL(changed(double, const std::string &)),
+            this, SLOT(OnForceChanged(double, const std::string &)));
+  }
+
+  frame->setLayout(vLayout);
+  frame->layout()->setContentsMargins(4, 0, 0, 0);
+  frame->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+  scrollArea->setWidget(frame);
+  scrollArea->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+  scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+
+  // Create a PID scroll area
+  QScrollArea *pidScrollArea = new QScrollArea;
+  QFrame *pidFrame = new QFrame;
+  pidFrame->setLineWidth(0);
+  pidFrame->setFrameShadow(QFrame::Sunken);
+  pidFrame->setFrameShape(QFrame::Box);
+  QVBoxLayout *vPIDLayout = new QVBoxLayout;
+  for (int i = 0; i < modelMsg.joint_size(); ++i)
+  {
+    std::string jointName = modelMsg.joint(i).name();
+    JointPIDControl *slider = new JointPIDControl(jointName, this);
+    this->pidSliders[jointName] = slider;
+    vPIDLayout->addWidget(slider);
+    connect(slider, SIGNAL(changed(double, const std::string &)),
+            this, SLOT(OnPIDChanged(double, const std::string &)));
+  }
+  pidFrame->setLayout(vPIDLayout);
+  pidFrame->layout()->setContentsMargins(4, 0, 0, 0);
+  pidFrame->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+  pidScrollArea->setWidget(pidFrame);
+  pidScrollArea->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+  pidScrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+  QTabWidget *tabWidget = new QTabWidget;
+  tabWidget->addTab(scrollArea, tr("Force"));
+  // tabWidget->addTab(pidScrollArea, tr("PID"));
+
+  // std::string title = "Model: ";
+  // title += _modelName;
+  // mainLayout->addWidget(new QLabel(tr(title.c_str())));
+  // mainLayout->addWidget(scrollArea);
+
+  // Add the the force and pid scroll areas to the tab
+  QVBoxLayout *mainLayout = new QVBoxLayout;
+  mainLayout->addWidget(tabWidget);
+  mainLayout->setContentsMargins(4, 4, 4, 4);
+
+  this->setLayout(mainLayout);
+  this->layout()->setSizeConstraint(QLayout::SetFixedSize);
+}
+
+/////////////////////////////////////////////////
+JointControlWidget::~JointControlWidget()
+{
+}
+
+/////////////////////////////////////////////////
+void JointControlWidget::OnForceChanged(double _value, const std::string &_name)
+{
+  std::map<std::string, JointForceControl*>::iterator iter;
+  iter = this->sliders.find(_name);
+  if (iter != this->sliders.end())
+  {
+    msgs::JointCmd msg;
+    msg.set_name(_name);
+    msg.set_force(_value);
+    this->jointPub->Publish(msg);
+  }
+}
+
+/////////////////////////////////////////////////
+void JointControlWidget::OnPIDChanged(double _value, const std::string &_name)
+{
+  std::map<std::string, JointForceControl*>::iterator iter;
+  iter = this->sliders.find(_name);
+  if (iter != this->sliders.end())
+  {
+    msgs::JointCmd msg;
+    msg.set_name(_name);
+    msg.set_position(_value);
+    this->jointPub->Publish(msg);
+  }
+}
+
+/////////////////////////////////////////////////
+void JointControlWidget::Load(const std::string &/*_modelName*/)
+{
+}
---
+++ src/gui/JointControlWidget.hh
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef JOINT_CONTROL_WIDGET_HH
+#define JOINT_CONTROL_WIDGET_HH
+
+#include <string>
+#include <map>
+#include "msgs/msgs.h"
+#include "gui/qt.h"
+#include "transport/TransportTypes.hh"
+
+namespace gazebo
+{
+  namespace gui
+  {
+    class JointForceControl;
+    class JointPIDControl;
+    class JointControlWidget : public QWidget
+    {
+      Q_OBJECT
+      public: JointControlWidget(const std::string &_model,
+                                 QWidget *_parent = 0);
+      public: virtual ~JointControlWidget();
+
+      public: void Load(const std::string &_modelName);
+      private slots: void OnForceChanged(double _value,
+                                         const std::string &_name);
+      private slots: void OnPIDChanged(double _value, const std::string &_name);
+
+      private: transport::NodePtr node;
+      private: transport::PublisherPtr jointPub;
+
+      private: msgs::Request *requestMsg;
+      private: std::map<std::string, JointForceControl*> sliders;
+      private: std::map<std::string, JointPIDControl*> pidSliders;
+    };
+
+    class JointForceControl : public QWidget
+    {
+      Q_OBJECT
+      public: JointForceControl(const std::string &_name, QWidget *_parent);
+      public slots: void OnChanged(double _value);
+      Q_SIGNALS: void changed(double /*_value*/, const std::string & /*_name*/);
+      private: std::string name;
+    };
+
+    class JointPIDControl : public QWidget
+    {
+      Q_OBJECT
+      public: JointPIDControl(const std::string &_name, QWidget *_parent);
+      public slots: void OnChanged(double _value);
+      Q_SIGNALS: void changed(double /*_value*/, const std::string & /*_name*/);
+      private: std::string name;
+    };
+  }
+}
+
+#endif
