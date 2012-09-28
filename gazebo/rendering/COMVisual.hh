5f192c880a20b1afce6a6e2d9e4905653b43bf0e
Nate Koenig <natekoenig@gmail.com>
2012-09-28 15:54:03 -0700

    Fixed ModelDatabase to first find local files before attempting to download

---
+++ gazebo/rendering/COMVisual.hh
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2011 Nate Koenig & Andrew Howard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+/* Desc: Center of Mass Visualization Class
+ * Author: Nate Koenig
+ */
+
+#ifndef COMVISUAL_HH
+#define COMVISUAL_HH
+
+#include <string>
+
+#include "rendering/Visual.hh"
+#include "msgs/msgs.hh"
+
+namespace ogre
+{
+  class SceneNode;
+}
+
+namespace gazebo
+{
+  namespace rendering
+  {
+    /// \addtogroup gazebo_rendering Rendering
+    /// \{
+
+    class DynamicLines;
+
+    /// \class COMVisual COMVisual.hh rendering/COMVisual.hh
+    /// \brief Basic Center of Mass visualization
+    class COMVisual : public Visual
+    {
+      public: COMVisual(const std::string &_name, VisualPtr _vis);
+      public: virtual ~COMVisual();
+
+      public: virtual void Load(sdf::ElementPtr _elem);
+      public: virtual void Load(ConstLinkPtr &_msg);
+
+      private: void Load(const math::Pose &_pose);
+
+      private: DynamicLines *crossLines;
+      private: Ogre::SceneNode *boxNode;
+    };
+    /// \}
+  }
+}
+#endif
