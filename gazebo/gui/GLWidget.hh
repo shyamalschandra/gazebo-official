001cfe3779ab761b355be8a57030c8ef89cdb822
Nate Koenig <natekoenig@gmail.com>
2013-05-14 07:27:27 -0700

    Updates

---
+++ gazebo/gui/GLWidget.hh
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef _GL_WIDGET_HH_
+#define _GL_WIDGET_HH_
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <list>
+
+#include "gui/qt.h"
+#include "rendering/RenderTypes.hh"
+
+#include "transport/TransportTypes.hh"
+
+#include "common/MouseEvent.hh"
+#include "common/Event.hh"
+
+#include "math/Pose.hh"
+
+#include "msgs/msgs.hh"
+
+#include "gui/BoxMaker.hh"
+#include "gui/SphereMaker.hh"
+#include "gui/CylinderMaker.hh"
+#include "gui/MeshMaker.hh"
+#include "gui/ModelMaker.hh"
+#include "gui/LightMaker.hh"
+
+namespace gazebo
+{
+  namespace gui
+  {
+    class GLWidget : public QWidget
+    {
+      Q_OBJECT
+
+      public: GLWidget(QWidget *_parent = 0);
+      public: virtual ~GLWidget();
+
+      public: void ViewScene(rendering::ScenePtr _scene);
+      public: rendering::UserCameraPtr GetCamera() const;
+      public: rendering::ScenePtr GetScene() const;
+
+      public: void Clear();
+
+      signals: void clicked();
+
+
+      protected: virtual void moveEvent(QMoveEvent *_e);
+      protected: virtual void paintEvent(QPaintEvent *_e);
+      protected: virtual void resizeEvent(QResizeEvent *_e);
+      protected: virtual void showEvent(QShowEvent *_e);
+      protected: virtual void enterEvent(QEvent * event);
+
+
+      protected: void keyPressEvent(QKeyEvent *_event);
+      protected: void keyReleaseEvent(QKeyEvent *_event);
+      protected: void wheelEvent(QWheelEvent *_event);
+      protected: void mousePressEvent(QMouseEvent *_event);
+      protected: void mouseDoubleClickEvent(QMouseEvent *_event);
+      protected: void mouseMoveEvent(QMouseEvent *_event);
+      protected: void mouseReleaseEvent(QMouseEvent *_event);
+
+      private: std::string GetOgreHandle() const;
+
+      /// \brief Callback for a mouse move event.
+      /// \param[in] _event The mouse move event
+      /// \return True if handled by this function.
+      private: bool OnMouseMove(const common::MouseEvent &_event);
+
+      /// \brief Process a normal mouse move event.
+      private: void OnMouseMoveNormal();
+
+      /// \brief Process an object translate mouse move event.
+      private: void OnMouseMoveTranslate();
+
+      /// \brief Process a make object mouse move event.
+      private: void OnMouseMoveMakeEntity();
+
+      /// \brief Callback for a mouse release event.
+      /// \param[in] _event The mouse release event
+      /// \return True if handled by this function.
+      private: bool OnMouseRelease(const common::MouseEvent &_event);
+
+      /// \brief Process a normal mouse release event.
+      private: void OnMouseReleaseNormal();
+
+      /// \brief Process an object translate mouse release event.
+      private: void OnMouseReleaseTranslate();
+
+      /// \brief Process a make object mouse release event.
+      private: void OnMouseReleaseMakeEntity();
+
+      /// \brief Callback for a mouse press event.
+      /// \param[in] _event The mouse press event
+      /// \return True if handled by this function.
+      private: bool OnMousePress(const common::MouseEvent &_event);
+
+      /// \brief Process a normal mouse press event.
+      private: void OnMousePressNormal();
+
+      /// \brief Process an object translate mouse press event.
+      private: void OnMousePressTranslate();
+
+      /// \brief Process a make object mouse presse event.
+      private: void OnMousePressMakeEntity();
+
+      private: void OnRequest(ConstRequestPtr &_msg);
+
+      private: void SmartMoveVisual(rendering::VisualPtr _vis);
+
+      private: void OnCreateScene(const std::string &_name);
+      private: void OnRemoveScene(const std::string &_name);
+      private: void OnMoveMode(bool _mode);
+      private: void OnCreateEntity(const std::string &_type,
+                                   const std::string &_data);
+      private: void OnFPS();
+      private: void OnOrbit();
+      private: void OnManipMode(const std::string &_mode);
+
+      private: void OnSetSelectedEntity(const std::string &_name,
+                                        const std::string &_mode);
+
+      private: void RotateEntity(rendering::VisualPtr &_vis);
+      private: void TranslateEntity(rendering::VisualPtr &_vis);
+
+      private: void OnMouseMoveVisual(const std::string &_visualName);
+      private: void OnSelectionMsg(ConstSelectionPtr &_msg);
+
+      private: bool eventFilter(QObject *_obj, QEvent *_event);
+      private: void PublishVisualPose(rendering::VisualPtr _vis);
+      private: void ClearSelection();
+
+      /// \brief Copy an object by name
+      private: void Paste(const std::string &_object);
+
+      private: void PushHistory(const std::string &_visName,
+                                const math::Pose &_pose);
+      private: void PopHistory();
+
+      /// \brief Set the selected visual, which will highlight the
+      /// visual
+      private: void SetSelectedVisual(rendering::VisualPtr _vis);
+
+      /// \brief Set the visual being moved, which will highlight the
+      /// visual
+      private: void SetMouseMoveVisual(rendering::VisualPtr _vis);
+
+      private: int windowId;
+
+      private: rendering::UserCameraPtr userCamera;
+      private: rendering::ScenePtr scene;
+      private: QFrame *renderFrame;
+      private: common::MouseEvent mouseEvent;
+
+      private: std::vector<event::ConnectionPtr> connections;
+
+      private: EntityMaker *entityMaker;
+      private: BoxMaker boxMaker;
+      private: SphereMaker sphereMaker;
+      private: CylinderMaker cylinderMaker;
+      private: MeshMaker meshMaker;
+      private: ModelMaker modelMaker;
+      private: PointLightMaker pointLightMaker;
+      private: SpotLightMaker spotLightMaker;
+      private: DirectionalLightMaker directionalLightMaker;
+
+      private: rendering::VisualPtr hoverVis, selectedVis, mouseMoveVis;
+
+      private: transport::NodePtr node;
+      private: transport::PublisherPtr modelPub, factoryPub;
+      private: transport::PublisherPtr lightPub;
+      private: transport::SubscriberPtr selectionSub, requestSub;
+
+      private: std::string keyText;
+      private: Qt::KeyboardModifiers keyModifiers;
+      private: QPoint onShiftMousePos;
+      private: math::Pose mouseMoveVisStartPose;
+
+      private: std::string copiedObject;
+
+      private: std::string state;
+
+      private: std::list<std::pair<std::string, math::Pose> > moveHistory;
+
+      /// \brief Callback to pause or enable rendering.
+      /// \param[in] _pause True to pause rendering.
+      private: void OnPauseRender(bool _pause);
+
+      /// \brief Flag that is set to true when GLWidget has responded to
+      ///  OnCreateScene
+      private: bool sceneCreated;
+
+      private: boost::mutex renderMutex;
+      private: bool pauseRendering;
+    };
+  }
+}
+
+#endif
---
+++ gazebo/gui/MainWindow.hh
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef _MAINWINDOW_HH_
+#define _MAINWINDOW_HH_
+
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+
+#include "gui/qt.h"
+#include "common/Event.hh"
+#include "msgs/MessageTypes.hh"
+#include "transport/TransportTypes.hh"
+
+namespace gazebo
+{
+  namespace gui
+  {
+    class RenderWidget;
+    class ToolsWidget;
+    class ModelListWidget;
+    class Editor;
+    class LogPlayWidget;
+
+    class MainWindow : public QMainWindow
+    {
+      Q_OBJECT
+
+      public: MainWindow();
+      public: virtual ~MainWindow();
+
+      public: void Load();
+      public: void Init();
+
+      public: unsigned int GetEntityId(const std::string &_name);
+      public: bool HasEntityName(const std::string &_name);
+
+      /// \brief Add a widget to the left column stack of widgets.
+      /// \param[in] _name Name of the widget
+      /// \param[in] _widget Pointer to the widget to add.
+      public: void AddToLeftColumn(const std::string &_name, QWidget *_widget);
+
+      /// \brief Show a widget in the left column.
+      /// \sa AddToLeftColumn
+      /// \param[in] _name Name of the widge to show. The widget must have
+      /// been added using AddToLeftColumn. The string "default" will show
+      /// the main tab.
+      public: void ShowLeftColumnWidget(const std::string &_name = "default");
+
+      /// \brief Get a pointer to the render widget.
+      /// \return A pointer to the render widget.
+      public: RenderWidget *GetRenderWidget() const;
+
+      /// \brief Play simulation.
+      public slots: void Play();
+
+      /// \brief Pause simulation.
+      public slots: void Pause();
+
+      protected: void closeEvent(QCloseEvent *_event);
+
+      private: void OnGUI(ConstGUIPtr &_msg);
+
+      private slots: void ItemSelected(QTreeWidgetItem *, int);
+      private slots: void New();
+      private slots: void Open();
+
+      /// \brief Open a log file
+      private slots: void OpenLog();
+
+      private slots: void Import();
+      private slots: void Save();
+      private slots: void SaveAs();
+      private slots: void About();
+      private slots: void Step();
+      private slots: void NewModel();
+      private slots: void Arrow();
+      private slots: void Translate();
+      private slots: void Rotate();
+
+      private slots: void CreateBox();
+      private slots: void CreateSphere();
+      private slots: void CreateCylinder();
+      private slots: void CreateMesh();
+      private slots: void CreatePointLight();
+      private slots: void CreateSpotLight();
+      private slots: void CreateDirectionalLight();
+
+      /// \brief Qt callback when the screenshot action is triggered
+      private slots: void CaptureScreenshot();
+
+      private slots: void InsertModel();
+      private slots: void ShowGrid();
+      private slots: void ShowCollisions();
+      private slots: void ShowJoints();
+      private slots: void ShowContacts();
+      private slots: void ShowCOM();
+      private slots: void Reset();
+      private slots: void FullScreen();
+      private slots: void FPS();
+      private slots: void Orbit();
+      private slots: void OnResetModelOnly();
+      private slots: void OnResetWorld();
+      private slots: void SetTransparent();
+      private slots: void SetWireframe();
+
+      /// \brief Qt call back when the play action state changes
+      private slots: void OnPlayActionChanged();
+
+      /// \brief QT slot to open the data logger utility
+      private slots: void DataLogger();
+
+      /// \brief Callback when topic selection action.
+      private slots: void SelectTopic();
+
+      /// \brief Callback for diagnostics action.
+      private slots: void Diagnostics();
+
+      /// \brief Receives status messages from the server.
+      /// \param[in] _msg Message pointer.
+      private: void OnServerStatus(ConstGzStringPtr &_msg);
+
+      private: void OnFullScreen(bool _value);
+      private: void OnMoveMode(bool _mode);
+
+      /// \brief Create most of the actions.
+      private: void CreateActions();
+
+      /// \brief Create menus.
+      private: void CreateMenus();
+
+      /// \brief Create the toolbars.
+      private: void CreateToolbars();
+
+      /// \brief Create the main menu bar.
+      private: void CreateMenuBar();
+
+      /// \brief Create all the editors.
+      private: void CreateEditors();
+
+      /// \brief Show a custom menubar. If NULL is used, the default menubar
+      /// is shown.
+      /// \param[in] _bar The menubar to show. NULL will show the default
+      /// menubar.
+      public: void ShowMenuBar(QMenuBar *_bar = NULL);
+
+      private: void OnModel(ConstModelPtr &_msg);
+      private: void OnResponse(ConstResponsePtr &_msg);
+      private: void OnWorldModify(ConstWorldModifyPtr &_msg);
+      private: void OnManipMode(const std::string &_mode);
+      private: void OnSetSelectedEntity(const std::string &_name,
+                                        const std::string &_mode);
+      private: void OnStats(ConstWorldStatisticsPtr &_msg);
+
+      /// \brief Handle event for changing the manual step size.
+      /// \param[in] _value New input step size.
+      private: void OnInputStepSizeChanged(int _value);
+
+      private: QToolBar *playToolbar;
+
+      private: RenderWidget *renderWidget;
+      private: ToolsWidget *toolsWidget;
+      private: ModelListWidget *modelListWidget;
+      private: LogPlayWidget *logPlay;
+
+      private: transport::NodePtr node;
+      private: transport::PublisherPtr worldControlPub;
+      private: transport::PublisherPtr serverControlPub;
+      private: transport::PublisherPtr selectionPub;
+      private: transport::PublisherPtr requestPub;
+      private: transport::PublisherPtr scenePub;
+
+      /// \brief Receives status messages from the server
+      private: transport::SubscriberPtr serverControlSub;
+      private: transport::SubscriberPtr responseSub;
+      private: transport::SubscriberPtr guiSub;
+      private: transport::SubscriberPtr newEntitySub, statsSub;
+      private: transport::SubscriberPtr worldModSub;
+
+      private: QDockWidget *toolsDock;
+
+      private: std::vector<event::ConnectionPtr> connections;
+
+      // A map that associates physics_id's with entity names
+      private: std::map<std::string, unsigned int> entities;
+
+      /// \brief Message used to field requests.
+      private: msgs::Request *requestMsg;
+
+      /// \brief The left-hand tab widget
+      private: QTabWidget *tabWidget;
+
+      /// \brief Mainwindow's menubar
+      private: QMenuBar *menuBar;
+
+      /// \brief A layout for the menu bar.
+      private: QHBoxLayout *menuLayout;
+
+      /// \brief Used to control size of each pane.
+      private: QStackedWidget *leftColumn;
+
+      /// \brief Map of names to widgets in the leftColumn QStackedWidget
+      private: std::map<std::string, int> leftColumnStack;
+
+      /// \brief The filename set via "Save As". This filename is used by
+      /// the "Save" feature.
+      private: std::string saveFilename;
+
+      /// \brief User specified step size for manually stepping the world
+      private: int inputStepSize;
+
+      /// \brief List of all the editors.
+      private: std::list<Editor*> editors;
+    };
+  }
+}
+#endif
