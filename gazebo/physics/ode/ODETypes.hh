d0bd9b6fab0639846b3611b82113954ed26a07ff
Steven Peters <scpeters@osrfoundation.org>
2014-03-12 14:35:08 -0700

    Merged in hidden_symbols (pull request #998)
    Use hidden symbol visibility by default

--- gazebo/physics/ode/ODETypes.hh
+++ gazebo/physics/ode/ODETypes.hh
@@ -25,6 +25,7 @@ namespace gazebo
   namespace physics
   {
     class ODECollision;
+    class ODEJoint;
     class ODELink;
     class ODERayShape;
     class ODESurfaceParams;
@@ -33,6 +34,10 @@ namespace gazebo
     /// \def ODECollisionPtr
     /// \brief Boost shared point to ODECollision
     typedef boost::shared_ptr<ODECollision> ODECollisionPtr;
+
+    /// \def ODEJointPtr
+    /// \brief Boost shared point to ODEJoint
+    typedef boost::shared_ptr<ODEJoint> ODEJointPtr;
 
     /// \def ODELinkPtr
     /// \brief Boost shared point to ODELink
