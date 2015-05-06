1783a3a0260e8f88b482738fd7e9c3b3ffee5aa6
Hugo Boyer <hugo@osrfoundation.org>
2015-05-06 10:56:17 -0700

    fix warning issue, removed error trace

---
+++ plugins/rest_web/RestUiWidget.cc
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <curl/curl.h>
+#include <QMessageBox>
+#include "RestUiWidget.hh"
+
+using namespace gazebo;
+
+/////////////////////////////////////////////////
+RestUiWidget::RestUiWidget(QWidget *_parent,
+                           const std::string &_menuTitle,
+                           const std::string &_loginTitle,
+                           const std::string &_urlLabel,
+                           const std::string &_defautlUrl)
+  : QWidget(_parent),
+    title(_menuTitle),
+    node(new gazebo::transport::Node()),
+    logoutDialog(this, _defautlUrl),
+    loginDialog(this, _loginTitle, _urlLabel, _defautlUrl)
+{
+  this->node->Init();
+  this->loginPub = node->Advertise<gazebo::msgs::RestLogin>(
+      "/gazebo/rest/rest_login");
+  this->logoutPub = node->Advertise<gazebo::msgs::RestLogout>(
+      "/gazebo/rest/rest_logout");
+  // this for a problem where the server cannot subscribe to the topic
+//  this->loginPub->WaitForConnection();
+  this->errorSub = node->Subscribe("/gazebo/rest/rest_error",
+                              &RestUiWidget::OnResponse,
+                              this);
+}
+
+/////////////////////////////////////////////////
+void RestUiWidget::Logout()
+{
+  if (this->logoutDialog.exec() != QDialog::Rejected)
+  {
+    gazebo::msgs::RestLogout msg;
+    std::string url = this->loginDialog.GetUrl();
+    msg.set_url(url);
+    gzmsg << "Loging out from: " << url << std::endl;
+    this->logoutPub->Publish(msg);
+  }
+}
+
+/////////////////////////////////////////////////
+void RestUiWidget::Login()
+{
+  if (this->loginDialog.exec() != QDialog::Rejected)
+  {
+    gazebo::msgs::RestLogin msg;
+    msg.set_url(this->loginDialog.GetUrl());
+    msg.set_username(this->loginDialog.GetUsername());
+    msg.set_password(this->loginDialog.GetPassword());
+    this->loginPub->Publish(msg);
+  }
+}
+
+/////////////////////////////////////////////////
+void RestUiWidget::OnResponse(ConstRestErrorPtr &_msg)
+{
+  gzerr << "Error received:" << std::endl;
+  gzerr << " type: " << _msg->type() << std::endl;
+  gzerr << " msg:  " << _msg->msg() << std::endl;
+  // add msg to queue for later processing from the GUI thread
+  this->msgRespQ.push_back(_msg);
+}
+
+/////////////////////////////////////////////////
+void RestUiWidget::Update()
+{
+  // Login problem?
+  while (!this->msgRespQ.empty())
+  {
+    std::string titleStr = this->title;
+    ConstRestErrorPtr msg = this->msgRespQ.front();
+    std::string msgStr = msg->msg();
+    this->msgRespQ.pop_front();
+    if (msg->type() == "Error")
+    {
+      msgStr += "\n\nIf the server is not available, ";
+      msgStr += "logout to hide theses messages.";
+      QMessageBox::critical(this,
+                            tr(titleStr.c_str()),
+                            tr(msgStr.c_str()));
+    }
+    else
+    {
+      QMessageBox::information(this,
+                               tr(titleStr.c_str()),
+                               tr(msgStr.c_str()));
+    }
+  }
+}
